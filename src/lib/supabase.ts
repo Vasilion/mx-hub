5417197ae5adea86a816110c1e1c24277bf86a30
Luke Vasilion <lukevasilion@gmail.com>
2025-03-27 01:06:53 -0400

    debug run 2

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,7 +4,7 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Database } from "@/types/supabase";
 
 export const supabase = createClientComponentClient<Database>({
-  supabaseUrl: "https://dxdoxsiegyvdooogbe.supabase.co",
+  supabaseUrl: "https://dxdoxsiegayrdsooogbe.supabase.co",
   supabaseKey:
     "NeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4ZG94c2llZ3l2ZG9vb2diZSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzA5MDk0MjY5LCJleHAiOjIwMjQ2NzAyNjl9.ZStlnJlZiiGmR42G94c2JF2cFsmRzb29Z",
 });
